646f4557f16161dd87df51b6a5e886c53828c1a5
Weslei Santos <88506317+WesleiSantos13@users.noreply.github.com>
2026-04-28 11:07:07 -0300

    Update participantes_routes.py

--- app/routes/participantes_routes.py
+++ app/routes/participantes_routes.py
@@ -6,3 +6,29 @@ participantes_bp = Blueprint("participantes", __name__)
 @participantes_bp.route("/participantes", methods=["GET"])
 def get_participantes():
     return jsonify(listar_participantes())
+
+
+# LISTAR POR ANO
+@participantes_bp.route("/participantes/ano/<int:ano>", methods=["GET"])
+def participantes_por_ano(ano):
+    dados = listar_participantes()
+
+    resultado = []
+    for p in dados:
+        if p["ano"] == ano:
+            resultado.append(p)
+
+    return jsonify(resultado)
+
+
+#Buscar participações por nome do time
+@participantes_bp.route("/participantes/time/<nome>", methods=["GET"])
+def participante_por_time(nome):
+    dados = listar_participantes()
+
+    resultado = []
+    for p in dados:
+        if nome.lower() in p["time"].lower():
+            resultado.append(p)
+
+    return jsonify(resultado)
