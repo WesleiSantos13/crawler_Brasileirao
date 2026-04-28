f193bd276445f96d2b2857d7cbad04dd745c3228
Weslei Santos <88506317+WesleiSantos13@users.noreply.github.com>
2026-04-28 10:39:01 -0300

    UPDATE

--- app/routes/confrontos_routes.py
+++ app/routes/confrontos_routes.py
@@ -2,6 +2,9 @@ from flask import Blueprint, jsonify, request
 from app.services.confrontos_service import listar_confrontos
 from operator import itemgetter
 
+from app.database.connection import SessionLocal
+from app.database.models import Time
+
 confrontos_bp = Blueprint("confrontos", __name__)
 
 #  LISTAR TUDO
@@ -26,17 +29,32 @@ def confrontos_por_ano(ano):
 
 
 # JOGOS COM MAIS GOLS
+
 @confrontos_bp.route("/confrontos/top-gols", methods=["GET"])
 def confrontos_mais_gols():
     top = request.args.get("top", 5, type=int)
 
     dados = listar_confrontos()
+    db = SessionLocal()
 
-    # cria campo total de gols
+    # cache pra evitar várias consultas repetidas
+    cache_times = {}
+
+    def get_nome_time(time_id):
+        if time_id not in cache_times:
+            time = db.query(Time).filter_by(id=time_id).first()
+            cache_times[time_id] = time.nome if time else None
+        return cache_times[time_id]
+
+    # adiciona nomes e total de gols
     for c in dados:
+        c["mandante"] = get_nome_time(c["mandante_id"])
+        c["visitante"] = get_nome_time(c["visitante_id"])
         c["total_gols"] = c["gols_mandante"] + c["gols_visitante"]
 
-    # ordena pelos jogos com mais gols
+    db.close()
+
+    # ordena
     dados.sort(key=itemgetter("total_gols"), reverse=True)
 
     return jsonify(dados[:top])
