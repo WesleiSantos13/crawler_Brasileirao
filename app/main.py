7d3bcbeeac74e1dbeb74c556e19a0e83fed0d8e7
Weslei Santos <88506317+WesleiSantos13@users.noreply.github.com>
2026-05-25 02:23:05 -0300

    front

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from app.routes.confrontos_routes import confrontos_bp
 from app.routes.times_routes import times_bp
@@ -9,8 +9,10 @@ from app.routes.hattricks_routes import hattricks_bp
 from app.routes.classificacao_routes import classificacao_bp
 
 def create_app():
-    app = Flask(__name__)
+    
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
+  
     app.register_blueprint(confrontos_bp)
     app.register_blueprint(times_bp)
     app.register_blueprint(participantes_bp)
@@ -19,5 +21,9 @@ def create_app():
     app.register_blueprint(hattricks_bp)
     app.register_blueprint(classificacao_bp)
 
+    
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
     return app
